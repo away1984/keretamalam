7a5bd74f9e1300b55eb5aa94317628d79e6ffd6c
away1984 <34096278+away1984@users.noreply.github.com>
2017-12-01 17:16:03 +0800

    Add files via upload

--- safelinkv2_utama.js
+++ safelinkv2_utama.js
@@ -26,7 +26,7 @@ if(x=="2"){xxx="11/do-you-forecast-forex-or-use-reality.html"}
 if(x=="3"){xxx="11/forex-swing-trading-strategies.html"}
 if(x=="4"){xxx="11/money-management-secret-to-success-in.html"}
 if(x=="5"){xxx="11/how-to-build-winning-trading-plan.html"}
-if(x=="6"){xxx="11/forex-social-platforms.html"}a_to_ve[i].href="http://www.keretamalam.cf/2017/"+xxx+"?url="+ encryptedUrl;a_to_ve[i].rel="nofollow";a_to_vb++;a_to_vc+=i+":::"+ a_to_ve[i].href+"\n";}}
+if(x=="6"){xxx="11/forex-social-platforms.html"}a_to_ve[i].href="https://linkpenyelamat.blogspot.com/2017/"+xxx+"?url="+ encryptedUrl;a_to_ve[i].rel="nofollow";a_to_vb++;a_to_vc+=i+":::"+ a_to_ve[i].href+"\n";}}
 var a_to_vj=document.getElementById("anonyminized");var a_to_vk=document.getElementById("found_links");if(a_to_vj)
 {a_to_vj.innerHTML+=a_to_vb;}
 if(a_to_vk)
